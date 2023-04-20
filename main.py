99556e8458de37932d531f86762844621c469eec
un4kn0wl3z <admin@unknownclub.net>
2023-04-20 10:48:45 +0700

    feat: single instance panel

--- main.py
+++ main.py
@@ -10,13 +10,32 @@ def on_exit():
     socket_io_client.stop()
     #print("Exiting the application...")
 
+
+import win32event
+import win32api
+import winerror
+
 app = QtWidgets.QApplication(sys.argv)
 qdarktheme.setup_theme()
 app.aboutToQuit.connect(on_exit)
 
 socket_io_client = SocketIOClient()
 window = MainWindow(app, socket_io_client)
-window.show()
 
+mutex = win32event.CreateMutex(None, False, "PattayaPanelSingleInstanceMutex")
+if not mutex:
+    QtWidgets.QMessageBox.warning(window,
+    "Pattaya Panel Alert",
+    "Cannot create Mutex",
+    QtWidgets.QMessageBox.Ok)
+    sys.exit(0)
+
+if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
+    QtWidgets.QMessageBox.warning(window, 
+    "Pattaya Panel Alert",
+    "One Instance only",
+    QtWidgets.QMessageBox.Ok)
+    sys.exit(0)
+window.show()
 app.exec()
 
